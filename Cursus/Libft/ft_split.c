8d5fdeed80cce6e3aae6abfa5d10779ab48a97e8
Angeles Casado Escano <angcasad@c1r8s2.42malaga.com>
2025-12-10 20:42:23 +0100

    SPLIIIT UPDATEEED

--- Cursus/Libft/ft_split.c
+++ Cursus/Libft/ft_split.c
@@ -1,92 +1,118 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angcasad <angcasad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/08 17:46:43 by angcasad          #+#    #+#             */
-/*   Updated: 2025/12/09 15:03:06 by angcasad         ###   ########.fr       */
+/*   Created: 2025/12/10 11:35:13 by angcasad          #+#    #+#             */
+/*   Updated: 2025/12/10 19:13:02 by angcasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdio.h>
-size_t	ft_counter(char const *s, char c)
+#include	"libft.h"
+//#include	<stdio.h>
+static void	*free_all(char **matrix)
 {
-	size_t	count;
 	int	i;
 	
-	count = 0;
 	i = 0;
-	while(s[i])
+	while(matrix[i] != NULL)
 	{
-		if(s[i] == c)
-			count++;
+		free(matrix[i]);
 		i++;
 	}
-	return(count + 1);
-}
-void	*free_all(char **strf, int i)
-{
-	while(!strf)
-	{
-		free(strf[i]);
-			i--;
-	}
-	free(strf);
+	free(matrix);
 	return(NULL);
+}
+static size_t	wordcount(char const *s, char c)
+{
+	size_t	i;
+	size_t	counter;
+
+	i = 0;
+	counter = 0;
+	while(s[i] != '\0')
+	{
+		while(s[i] != '\0' && s[i] == c)
+			i++;
+		if(s[i] != '\0' && s[i] != c)
+			counter++;
+		while(s[i] != '\0' && s[i] != c)
+			i++;
+	}
+	return(counter);
+}
+
+static size_t	charcount(char const *s, char c)
+{
+	size_t	i;
+	size_t	final;
+
+	i = 0;
+	final = 0;
+	
+	while(s[i] != '\0' && s[i] == c)
+			i++;
+		if(s[i] != '\0' && s[i] != c)
+		{
+			while(s[i] != '\0' && s[i] != c)
+			{
+				i++;
+				final++;
+			}
+		}
+	return(final);
 }
 char	**ft_split(char const *s, char c)
 {
-	size_t	size;
+	char	**matrix;
+	size_t	i;
+	size_t	j;
+	size_t	n_words;
 	size_t	start;
 	size_t	len;
-	size_t	i;
-	size_t	n_obj;
-	char	**matrix;
+	
+	if(!s)
+		return(NULL);
 	i = 0;
-	size = ft_strlen(s);
+	j = 0;
 	start = 0;
-	n_obj = ft_counter(s,c);
-	matrix = malloc((n_obj + 1) * sizeof(char*));
+	n_words = wordcount(s, c) ;
+	matrix = malloc((n_words + 1 )* sizeof(char *));
 	if(!matrix)
 		return(NULL);
-	while(i < n_obj)
-	{
-		//len = ft_strlen(s + start) - ft_strlen(ft_strchr((s + start),c));
-		len = ft_strlen(s + start) - ft_strlen(ft_strchr((s + start),c));
-		//printf("%s\n", s + start);
-		//printf("%zu\n", start);
-
-		matrix[i] = ft_substr(s,start,len);
-			if(!&matrix[i])
-				free_all(matrix, i);
-		start = start + (len + 1);
+	while(i < n_words)
+	{	
+		while(s[start] == c)
+			start++;
+		len = charcount((s + start), c);
+		matrix[i] = ft_substr(s, start, len);
+		if(!matrix[i])
+			free_all(matrix);
 		i++;
+		start += len;
 	}
-	matrix[n_obj] = NULL;
+	matrix[i] = NULL;
 	return(matrix);
 }
-
-int	main(void)
+/*int	main(void)
 {
-	char	*s;
-	char	**matrix;
-	char	c;
 	int	i;
-	
+	char	**matrix;
+	char	*s;
+
+	s = NULL;
 	i = 0;
-	c = ' ';
-	s = "no va a funcionar";
-	matrix = ft_split(s, c);
+	matrix = ft_split1(s, ' ');
 	if(!matrix)
 		return(1);
+			//freeall?
 	while(matrix[i] != NULL)
 	{
-		printf("%s\n", matrix[i]);
+		printf("%s\n",matrix[i]);
 		i++;
 	}
-	free_all(matrix, i);
+	free_all(matrix);
 	return(0);
-}
+}*/
