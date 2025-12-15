55dcb8a15d4fc9acec60665973812e30d2bfbbe7
Angeles Casado Escano <angcasad@c1r3s3.42malaga.com>
2025-12-15 16:28:57 +0100

    itoa updated

--- Cursus/Libft/ft_itoa.c
+++ Cursus/Libft/ft_itoa.c
@@ -6,52 +6,82 @@
 /*   By: angcasad <angcasad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 19:17:51 by angcasad          #+#    #+#             */
-/*   Updated: 2025/12/11 14:00:53 by angcasad         ###   ########.fr       */
+/*   Updated: 2025/12/15 16:26:04 by angcasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	ft_numb_count(int n)
 {
 	int	x;
+	int	neg;
 
 	x = 0;
-	if(n <= 9)
-		return(1);
-	while(n > 9)
-		n = n / 10;
-	x++;
-}
-char	ft_big_number(int n, int *i)
-{
+	if(n < 0)
+	{
+		neg = -1;
+		n = n * neg;
+		x++;
+	}
 	while(n > 9)
 	{
-		ft_big_number(n / 10); //1
-		ft_big_number(n % 10);
+		n = n / 10;
+		x++;
 	}
-	*i++;
-	return(n + '0');
+	if(n <= 9)
+		return(x + 1);
+	return(x + 1);
 }
-
-char	ft_itoa(int n)
+void	ft_changer(int n, int *i, char *str)
+{
+	if(n > 9)
+	{
+		ft_changer(n / 10, i, str);
+	}
+	
+	str[*i] = n % 10 + '0';
+	
+	(*i)++;
+	return;
+}
+char	*ft_itoa(int n)
 {
 	int	i;
 	char	*str;
 	int	nc;
+	int	neg;
 
+	neg = -1;
+	
 	nc = ft_numb_count(n);
 	str = malloc((nc + 1) * sizeof(char));
 		if(!str)
 			return(NULL);
-	while(n > 9)
+	i = 0;
+	if(n == -2147483648)
 	{
-		str[i] = ft_big_number(n);
-		i++;
+		str = "-2147483648";
+		return(str);
 	}
-	
+	if(n < 0)
+	{
+		n = n * neg;
+		i++;
+		str[0] = '-';
+	}
+	/*if(n <= 9)
+	str[i] = n + '0';*/
+	ft_changer(n, &i, str);	
+	str[nc] = '\0';
+	return(str);
 }
 int	main(void)
 {
+	char	*str;
 	
+	str = ft_itoa(-2147483648);
+	printf("%s\n",str);
+	free(str);
 }
