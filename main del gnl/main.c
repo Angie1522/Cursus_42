e4d5d815fb30cb59738f5338bfce1745007b2629
Angeles Casado Escano <angcasad@c3r10s3.42malaga.com>
2026-02-25 11:23:24 +0100

    gnl terminado

---
+++ main del gnl/main.c
@@ -0,0 +1,24 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+    int     fd;
+    char    *line;
+
+    fd = open("hola.txt", O_RDONLY);
+    if (fd < 0)
+        return (1);
+
+    line = get_next_line(fd);
+    while (line)
+    {
+        printf("%s", line);
+        free(line);
+        line = get_next_line(fd);
+    }
+    close(fd);
+    free(line);
+    return (0);
+}
