14695f3b2ba5bacbf589da25ebf519e8b9054a49
Angie Casado <mangelescasado97@gmail.com>
2026-02-28 22:21:44 +0100

    Crear ft_garden_summary.py

---
+++ Python/Growing_code/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+	garden = input("Enter garden name: ")
+    number = input("Enter number of plants: ")
+    print("Garden:", garden)
+    print("Plants:", number)
+    print("Status: Growing well!")
