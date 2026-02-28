c3cb34e9869efaa928a6d9bd9ae7aa283bdf95e6
Angie Casado <mangelescasado97@gmail.com>
2026-02-28 21:45:18 +0100

    Actualizar ft_count_harvest_iterative.py

--- Python/Growing_code/ft_count_harvest_iterative.py
+++ Python/Growing_code/ft_count_harvest_iterative.py
@@ -1,2 +1,5 @@
 def ft_count_harvest_iterative():
-    ttal_days = int(input(""))
+    total_days = int(input("Days until harvest: "))
+    for day in range(1, total_days + 1)
+    	print("Day", day)
+    print("Harvest time!")
