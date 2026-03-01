6d2dccfd490274548ab47670cf87f0c3c99eaad0
Angie Casado <mangelescasado97@gmail.com>
2026-03-01 22:15:21 +0100

    Actualizar more_operations.c

--- Push_swap/srcs/more_operations.c
+++ Push_swap/srcs/more_operations.c
@@ -10,3 +10,38 @@ void	pa(t_node **a, t_node **b)
     *a = tmp;//ahora guardamos nuestro antiguo top de b modificado en a.
     write(1,"pa\n", 3);
 }
+
+void	rotate(t_node **stack)
+{
+    t_node	*tmp;
+    t_node	*last;
+    
+    if(!*stack || !(*stack)->next)
+    	return;
+    tmp = *stack;//guardamos el top
+    *stack = (*stack)->next;//stack empieza en el 2º
+    last = *stack;//guardamos en last el top anterior
+    while(last->next != NULL)//avanzamos en last hasta el ultimo nodo de stack
+    	last = last->next;
+    last->next = tmp;
+    tmp->next = NULL;
+}
+
+void	ra(t_node **a)
+{
+    rotate(a);
+    write(1,"ra\n",3);
+}
+
+void	rb(t_node **b)
+{
+    rotate(b);
+    write(1,"rb\n",3);
+}
+void	rr(t_node **a, t_node **b)
+{
+    rotate(a);
+    rotate(b);
+    write(1,"rr\n",3);
+}
+    
