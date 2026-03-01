a13e0d4b50de9f8a1feabfcd1e37a8305d61c2f0
Angie Casado <mangelescasado97@gmail.com>
2026-03-01 16:37:53 +0100

    Actualizar push_swap.h

--- Push_swap/push_swap.h
+++ Push_swap/push_swap.h
@@ -1,5 +1,18 @@
-typedef struct	s_node
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_node
 {
-	int	value;
-    struct s_node	*next;
-} t_node;
+    int            value;
+    struct s_node  *next;
+}   t_node;
+
+// stack_utils.c
+t_node  *new_node(int value);
+void    add_to_bottom(t_node **stack, int value);
+int     stack_size(t_node *stack);
+
+#endif
