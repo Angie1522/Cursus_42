8bc439467fe4a7cd10379ae32db35f33f75c852e
Angie Casado <mangelescasado97@gmail.com>
2026-03-01 18:00:26 +0100

    Actualizar push_swap.h

--- Push_swap/push_swap.h
+++ Push_swap/push_swap.h
@@ -1,10 +1,10 @@
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef PUSH_SWAP_H//si push swap no esta definido... (evita que el archivo se incluya dos veces)
+# define PUSH_SWAP_H//...incluyelo ahora.(la proxima vez que alguien intente incluir el header ya esta definido y no lo procesa dos veces.)
 
-# include <stdlib.h>
-# include <unistd.h>
+# include <stdlib.h>//para poder usar malloc y free
+# include <unistd.h>//para poder usar write
 
-typedef struct s_node
+typedef struct s_node//nuestra estructura para crear nodos
 {
     int            value;
     struct s_node  *next;
@@ -15,4 +15,4 @@ t_node  *new_node(int value);
 void    add_to_bottom(t_node **stack, int value);
 int     stack_size(t_node *stack);
 
-#endif
+#endif//cierra el ifndef. Las tres líneas juntas se llaman include guard.
